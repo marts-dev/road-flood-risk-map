e75c7d13b5f215c4e82b3c6117a02fc72f69968d
marts-dev <8363978+marts-dev@users.noreply.github.com>
2025-09-15 03:59:38 +0800

    Bump version: 0.1.1 → 0.1.2

--- road_flood_risk_map/__init__.py
+++ road_flood_risk_map/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Martin Arrogante"""
 __email__ = "arrogantemartin@gmail.com"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from road_flood_risk_map.road_flood_risk_map import RoadFloodRiskMap
 from road_flood_risk_map.common import compute_d8_direction, fill_depressions, fill_depressions_flow_dirs, fill_depression_epsilon
