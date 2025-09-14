9bf9f06f11c673d40f2bbfdf7edbc0f31462401f
marts-dev <8363978+marts-dev@users.noreply.github.com>
2025-09-15 02:44:32 +0800

    Refactor and enhance road flood risk mapping functionality
    - Deleted the old introductory notebook (intro.ipynb) and added a new introduction notebook (introduction.ipynb) with updated content and examples.
    - Updated requirements.txt to ensure ipywidgets is included.
    - Modified __init__.py to import additional functions from the common module.
    - Enhanced common.py with improved functions for computing D8 flow direction, filling depressions, and handling raster metadata.
    - Updated road_flood_risk_map.py to include a new analysis widget for user interaction, allowing for the selection of data sources and file names for hydrological analysis.
    - Improved the constructor of RoadFloodRiskMap to accept a basemap parameter and updated the method for performing hydrological analysis to return relevant file names.

--- road_flood_risk_map/__init__.py
+++ road_flood_risk_map/__init__.py
@@ -5,4 +5,4 @@ __email__ = "arrogantemartin@gmail.com"
 __version__ = "0.0.2"
 
 from road_flood_risk_map.road_flood_risk_map import RoadFloodRiskMap
-from road_flood_risk_map.common import compute_d8_direction
+from road_flood_risk_map.common import compute_d8_direction, fill_depressions, fill_depressions_flow_dirs, fill_depression_epsilon
